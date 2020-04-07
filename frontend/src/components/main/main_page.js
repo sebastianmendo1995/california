f03163d4e2c719866f7959d9e17d3ea0855b1d75
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2020-04-07 13:47:16 -0500

    navbar

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>California Hotel</h1>
+                <footer>
+                    Copyright &copy; 2020 Hotel California
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
