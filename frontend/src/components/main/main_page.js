b8fb049e03416863f272fcbaa49ea07751bd154b
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2020-05-08 12:06:09 -0500

    Login and signup forms

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,7 +5,7 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>California Hotel</h1>
+                <h1>Main Page</h1>
                 <footer>
                     Copyright &copy; 2020 Hotel California
                 </footer>
