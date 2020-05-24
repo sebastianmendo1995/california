d8b264ad6e95718d3cdee68f1d633716293bd13c
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2020-05-24 18:52:48 -0500

    Adding error page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch, Route } from 'react-router-dom';
-import NavBarContainer from './nav/navbar_container';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
+import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
+import ErrorPage from './error_page/error_page'
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
@@ -14,11 +15,13 @@ const App = () => (
         <Switch>
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
+            <Route exact path="/errors" component={ErrorPage} /> 
             <Route path='/' component={NavBarContainer} />
         </Switch>
-        
+
         <Switch>
             <Route exact path="/" component={MainPage} />
+            <Redirect to='/errors' />
         </Switch>
     </div>
 );
---
+++ frontend/src/components/error_page/error_page.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './error.css'
+
+class ErrorPage extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <section className="errors-section">
+                <div id="notfound">
+                    <div className="notfound">
+                        <div className="notfound-404">
+                            <h1>Oops!</h1>
+                            <h2>404 - The Page can't be found</h2>
+                        </div>
+                        <Link to='/'> Go TO Homepage</Link>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+
+export default ErrorPage;
