04904095c7ce87111ff291fa337b72d2e1668441
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2020-05-30 19:05:14 -0500

    Main page

---
+++ frontend/src/components/main/main_page.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './main.css'
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div id='wrap'>
+                <div>
+                    <div className='container-large signed-out-main'>
+                        <div className='text-content'>
+                            <h1>Hospedarse en lima nunca fue tan barato</h1>
+                            <p>Para que gastes tu dinero donde realmente lo necesites</p>
+                        </div>
+                        <div className=''>
+                            
+                        </div>
+                        <a target="_blank" href="https://www.freepik.com/free-photos-vectors/business">Business vector created by pch.vector - www.freepik.com</a>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default MainPage;
