d835f1561eb5403fec7552e62ac3c2c4826f7008
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2020-06-05 02:16:17 -0500

    Main page shot content

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 // Libary Imports
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
 const bodyParser = require("body-parser");
 
 // File Import
-const users = require('./routes/api/users');
+const users = require("./routes/api/users");
 
 // SETUP
 const app = express();
-const PORT = process.env.PORT || 5000; 
-const db = require('./config/keys').mongoURI;
+const PORT = process.env.PORT || 5000;
+const db = require("./config/keys").mongoURI;
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to mongoDB"))
-    .catch(err => console.log(err))
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.log(err));
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 
 // Routes
 app.use("/api/users", users);
--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './main.css'
+import "./main.css";
 
 class MainPage extends React.Component {
-
-    render() {
-        return (
-            <div id='wrap'>
-                <div>
-                    <div className='container-large signed-out-main'>
-                        <div className='text-content'>
-                            <h1>Hospedarse en lima nunca fue tan barato</h1>
-                            <p>Para que gastes tu dinero donde realmente lo necesites</p>
-                        </div>
-                        <div className=''>
-                            
-                        </div>
-                        <a target="_blank" href="https://www.freepik.com/free-photos-vectors/business">Business vector created by pch.vector - www.freepik.com</a>
-                    </div>
-                </div>
-
+  render() {
+    return (
+      <div id="wrap">
+        <div className="main-page-content">
+          <div className="container-large signed-out-main">
+            <div className="text-content">
+              <h1>Hospedarse en lima nunca fue tan barato</h1>
+              <p>Para que gastes tu dinero donde realmente lo necesites</p>
             </div>
-        );
-    }
+            <div className="shot-content">
+              <div className='shot-image'></div>
+              <a
+                target="_blank"
+                href="https://www.freepik.com/free-photos-vectors/business"
+              >
+                Business vector created by pch.vector - www.freepik.com
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MainPage;
